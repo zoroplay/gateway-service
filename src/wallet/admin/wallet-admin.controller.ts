6672ec58769fc0bfdb464059ffeeacdf897da453
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-02-26 18:53:54 +0100

    updates

--- src/wallet/admin/wallet-admin.controller.ts
+++ src/wallet/admin/wallet-admin.controller.ts
@@ -1,8 +1,8 @@
 import { Body, Controller, Get, Inject, Param, Post, Query } from '@nestjs/common';
 import { ApiBody, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { ClientGrpc } from '@nestjs/microservices';
-import { PaymentMethodRequest, WALLET_SERVICE_NAME, WalletServiceClient, protobufPackage } from '../wallet.pb';
-import { SwaggerGetPaymentMethodResponse, SwaggerPaymentMethodRequest, SwaggerPaymentMethodResponse } from '../dto';
+import { ListWithdrawalRequests, PaymentMethodRequest, WALLET_SERVICE_NAME, WalletServiceClient, protobufPackage } from '../wallet.pb';
+import { SwaggerGetPaymentMethodResponse, SwaggerListWithdrawalRequests, SwaggerPaymentMethodRequest, SwaggerPaymentMethodResponse } from '../dto';
 
 @ApiTags('BackOffice APIs')
 @Controller('admin/wallet')
@@ -23,7 +23,7 @@ export class WalletAdminController {
     })
     @ApiBody({ type: SwaggerPaymentMethodRequest })
     @ApiOkResponse({ type: SwaggerPaymentMethodResponse })
-    saveRole(@Body() body: PaymentMethodRequest) {
+    savePaymentMethod(@Body() body: PaymentMethodRequest) {
         return this.svc.savePaymentMethod(body);
     }
 
@@ -43,6 +43,38 @@ export class WalletAdminController {
         @Query() query 
     ) {
         return this.svc.getPaymentMethods({clientId: param.client_id, status: query.status});
+    }
+
+    @Post('/payment-methods')
+    @ApiOperation({
+        summary: 'Save Payment Methods',
+        description: 'This endpoint is used to save or update payment methods for client',
+    })
+    @ApiBody({ type: SwaggerPaymentMethodRequest })
+    @ApiOkResponse({ type: SwaggerPaymentMethodResponse })
+    saveRole(@Body() body: PaymentMethodRequest) {
+        return this.svc.savePaymentMethod(body);
+    }
+
+    @Post('withdrawals')
+    @ApiOperation({
+        summary: 'Fetch Withdrawal Requests',
+        description: 'This endpoint is used to fetch all withdrawal requests',
+    })
+    @ApiBody({type: SwaggerListWithdrawalRequests})
+    @ApiOkResponse({ type: SwaggerGetPaymentMethodResponse })
+    withdrawals(
+        @Body() param: ListWithdrawalRequests,
+        @Query() query 
+    ) {
+
+        return this.svc.listWithdrawals({
+            clientId: param.clientId, 
+            from: param.from,
+            to: param.to,
+            status: query.status,
+            userId: param.userId
+        });
     }
 
 }
