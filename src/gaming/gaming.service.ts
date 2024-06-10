2ae2ae4f6dd708ce323cbd602a28ae573b374df3
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-06-10 14:28:24 +0100

    minor fix

--- src/gaming/gaming.service.ts
+++ src/gaming/gaming.service.ts
@@ -80,6 +80,11 @@ export class GamingService implements OnModuleInit {
     const resp = await firstValueFrom(this.service.handleCallback(request));
     console.log(resp);
     if (resp.success) {
+      if(resp.data.Amount) {
+        resp.data.Amount = parseFloat(resp.data.Amount.toFixed(2))
+      } else if (resp.data.Balance) {
+        resp.data.Balance = parseFloat(resp.data.Balance.toFixed(2))
+      }
       console.log('service ended in success');
     } else {
       console.log('service ended in failure');
