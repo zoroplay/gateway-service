090a71c7de37980a95f556e3c25d4af49c503b80
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-06-11 11:55:20 +0100

    changing smart soft balance

--- src/gaming/gaming.service.ts
+++ src/gaming/gaming.service.ts
@@ -83,16 +83,10 @@ export class GamingService implements OnModuleInit {
       if(request.provider === 'smart-soft' && request.action !== 'ActivateSession') {
         if(request.action === 'GetBalance') {
           console.log('formatting amount')
-          resp.data.Amount = parseFloat(resp.data.Amount.toFixed(2)).toLocaleString(undefined, {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          })
+          resp.data.Amount = this.formatNumber(resp.data.Amount)
         } else {
           console.log('formatting balance')
-          resp.data.Balance = parseFloat(resp.data.Balance.toFixed(2)).toLocaleString(undefined, {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          })
+          resp.data.Balance = this.formatNumber(resp.data.Balance)
         }
       }
       console.log('service ended in success');
@@ -153,4 +147,12 @@ export class GamingService implements OnModuleInit {
     if (res.status) res.data.balance = parseFloat(res.data.balance.toFixed(2));
     return res;
   }
+
+  formatNumber (num) {
+    if (num > 0 && num % 1 === 0) {
+      return parseFloat(num + ".00");
+    } else {
+      return parseFloat(num.toFixed(2))
+    }
+  };
 }
