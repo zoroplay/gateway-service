fde14f6d5960ab727424ec82606aa3d93e34f951
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-08-22 12:15:22 +0100

    updates for betting

--- src/interfaces/betting.pb.ts
+++ src/interfaces/betting.pb.ts
@@ -396,6 +396,7 @@ export interface BetHistory {
   markets: string;
   betCategoryDesc: string;
   isBonusBet?: boolean | undefined;
+  pendingGames?: number | undefined;
 }
 
 export interface BetHistoryResponse {
