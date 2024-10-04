60ae120cc5e7f7b89623a4d146bb02324aca750d
emmanuelben1999 <emmanuelben1999@gmail.com>
2024-10-04 13:40:51 +0100

    fix:added semantic release.exec

---
+++ release.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      [
+        '@semantic-release/exec',
+        {
+          verifyReleaseCmd: 'echo NEXT_VERSION=${nextRelease.version} >> .VERSION',
+        },
+      ],
+      [
+        '@semantic-release/gitlab',
+        {
+          assets: 'release/*.tgz',
+        },
+      ],
+  
+      [
+        '@semantic-release/npm',
+        {
+          npmPublish: false,
+        },
+      ],
+      '@semantic-release/git',
+    ],
+    preset: 'angular',
+  };
+  
