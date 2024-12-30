ce6f0dcf38e9fb7424e762b6b3264dd4c50732cf
Isoboye Ibanibo <elsopi02@yahoo.com>
2024-12-30 17:42:02 +0100

    added auth guard to place-bet endpoint

--- src/betting/betting.controller.ts
+++ src/betting/betting.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Ip,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import {
   ApiBody,
@@ -43,6 +44,7 @@ import {
   UpdateBetRequest,
 } from '../interfaces/betting.pb';
 import { SwaggerCommonResponse } from 'src/identity/dto';
+import { AuthGuard } from 'src/identity/auth/auth.guard';
 
 @ApiTags('Betting APIs')
 @Controller('bets')
@@ -114,6 +116,7 @@ export class BettingController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Post('/place-bet/:client_id')
   @ApiOperation({
     summary: 'Place a bet request',
@@ -124,6 +127,32 @@ export class BettingController {
   @ApiBody({ type: SwaggerPlaceBet })
   @ApiOkResponse({ type: SwaggerPlaceBetResponse })
   PlaceBet(
+    @Body() data, 
+    @Param() param: any, 
+    @Ip() ip: any
+  ) {
+    try {
+      data.clientId = param.client_id;
+      data.ipAddress = ip;
+      data.betType = data.bet_type;
+      data.type = data.event_type;
+      // console.log(data);
+      return this.bettingService.PlaceBet(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @Post('/book-bet/:client_id')
+  @ApiOperation({
+    summary: 'Book a bet request',
+    description:
+      'Receives a bet request with all the required detailed, upon successful bet placement, unique betID is returned',
+  })
+  @ApiParam({ name: 'client_id', type: 'number' })
+  @ApiBody({ type: SwaggerPlaceBet })
+  @ApiOkResponse({ type: SwaggerPlaceBetResponse })
+  BookBet(
     @Body() data, 
     @Param() param: any, 
     @Ip() ip: any
