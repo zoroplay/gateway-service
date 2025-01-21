5fe70cb2a5ee881b990d3de35473cf705055b859
Isoboye Ibanibo <elsopi02@yahoo.com>
2025-01-21 10:49:38 +0100

    fix for qtech endpoints

--- src/gaming/gaming.controller.ts
+++ src/gaming/gaming.controller.ts
@@ -27,8 +27,6 @@ import {
 import { Response } from 'express';
 import { GamingService } from './gaming.service';
 import {
-  FindOneCategoryDto,
-  QtechtransactionRequest,
   StartGameDto,
 } from 'src/interfaces/gaming.pb';
 import {
@@ -371,6 +369,55 @@ export class GamingController {
     }
   }
 
+  @Post(':clientId/transactions')
+  @ApiParam({ name: 'clientId', type: 'string' })
+  @ApiHeader({
+    name: 'Wallet-Session',
+    description: 'Session signature for validation',
+  })
+  @ApiHeader({
+    name: 'Pass-Key',
+    description: 'Pass Key for authentication',
+  })
+  async QtechTransaction(
+    @Headers() headers: Record<string, string>,
+    @Res() res: Response,
+    @Param('clientId') clientId: number,
+    @Body() data: Record<string, any>,
+  ) {
+
+    try {
+
+      console.log('Hit Transaction');
+
+      const response = await this.gamingService.handleQtechGamesCallback({
+        playerId: data.playerId,
+        gameId: data.gameId,
+        walletSessionId: headers['wallet-session'],
+        passkey: headers['pass-key'],
+        body: Object.keys(data).length === 0 ? '' : JSON.stringify(data),
+        clientId,
+        action: data.txnType,
+      });
+
+      if (!response.success) {
+        return res
+          .status(response.status)
+          .send(response.data);
+      }
+
+      return res.status(response.status).send(response.data);
+      
+    } catch (error) {
+      console.error('Error in QtechBet:', error);
+      return res.status(500).json({
+        code: "UNKNOWN_ERROR",
+        message:
+          'An unexpected error occurred while processing the transaction.',
+      });
+    }
+  }
+
   @Post('/transactions/rollback')
   @ApiHeader({ name: 'Wallet-Session', description: 'Signature' })
   @ApiHeader({ name: 'Pass-Key', description: 'Pass Key' })
@@ -380,7 +427,13 @@ export class GamingController {
     @Req() request: Request,
     @Body() data: Record<string, any>,
   ) {
-
+    console.log({
+      message: 'CHECKING-ROLLBACK',
+      playerId: data.playerId,
+      method: request.method,
+      header: headers,
+      body: data,
+    });
 
     const walletSessionId = headers['wallet-session'];
     const passkey = headers['pass-key'];
